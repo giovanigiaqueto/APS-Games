f38df27b3002e831967382f8fbbecf469841a4dd
Giovani Giaqueto <giovanigiaqueto@gmail.com>
2020-10-14 11:37:03 -0300

    src/carregamento movido para test-carregamento

--- src/carregamento/carregadores/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from .base import *
-
-from .auxiliares import *
-from .sequencias import *
--- src/carregamento/delimitadores.py
+++
@@ -1,7 +0,0 @@
-
-import re
-
-delim_virgula = re.compile(r'\s*,\s*')
-delim_dois_pontos = re.compile(r'\s*:\s*')
-delim_espaco = re.compile(r'\s+')
-delim_ponto = re.compile(r'.')
